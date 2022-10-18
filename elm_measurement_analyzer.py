7b0aa3f45f9c357cf877f4c70dc005399200cbe1
krezolekcoder <kkrezole@gmail.com>
2022-10-18 20:29:24 +0200

    Core app logic moved to separate class, code refactor

--- elm_measurement_analyzer.py
+++ elm_measurement_analyzer.py
@@ -1,7 +1,60 @@
-
+import csv 
 
 class ELM_MeasurementAnalyzer:
 
     def __init__(self, csv_filename: str):
-        pass 
+
+        measurement_dictionary = {} 
+
+        with open(csv_filename, newline='', encoding="utf-8") as csvfile:
+            trip_reader = list(csv.reader(csvfile, delimiter=';', quotechar='|'))
+        
+            # for dict_key in trip_reader[0]:
+            #     if(len(dict_key) > 0):
+            #         key = dict_key.split('"')[1]
+            #         measurement_dictionary[dict_key] = None
+
+            measurement_list_first = [] 
+            measurement_list_second = []
+            measurement_list_third = [] 
+            measurement_list_fourth = [] 
+
+            measurements_list_of_lists = [] 
+
+
+            for record in trip_reader[1:]:
+                measurement_list_first.append(record[0])
+                measurement_list_second.append(record[1])
+                measurement_list_third.append(record[2])
+                measurement_list_fourth.append(record[3])
+                
+            measurement_dictionary['SECONDS'] = measurement_list_first
+            measurement_dictionary['PID'] = measurement_list_second
+            measurement_dictionary['VALUE'] = measurement_list_third
+            measurement_dictionary['UNITS'] = measurement_list_fourth
+
+        # Get unique measurement types from whole measurement 
+        measurement_types = list(set(measurement_dictionary['PID']))
+
+
+        list_of_measurement_types = [[el] for el in measurement_types]
+
+        for meas_number, meas_name in enumerate(measurement_dictionary['PID']):
+            for pid_number, pid_meas_name in enumerate(measurement_types):
+                if pid_meas_name == meas_name:
+                    # extract time value and unit parameters from measurement 
+                    time =  round (float(measurement_dictionary['SECONDS'][meas_number].split('"')[1]) - float(measurement_dictionary['SECONDS'][0].split('"')[1]))
+                    value = float(measurement_dictionary['VALUE'][meas_number].split('"')[1])
+                    unit = measurement_dictionary['UNITS'][meas_number].split('"')[1]
+
+                    list_of_measurement_types[pid_number].append((time, value, unit))
+
+        for pid_measurement in list_of_measurement_types:
+            measurement_dictionary[pid_measurement[0]] = pid_measurement[1:]
+            
+        self.measurement =  dict( sorted(measurement_dictionary.items(), key=lambda x: x[0].lower()) ) 
+
     
+    def get_measurement(self) -> dict:
+
+        return self.measurement
--- main.py
+++ main.py
@@ -18,80 +18,20 @@ if __name__ == "__main__":
     for file in csv_filenames:
         filepaths.append(f'vw_golf_worktrips/{file}')
     
-    measurement_dictionary = {} 
+    meas_analyzer = ELM_MeasurementAnalyzer(filepaths[0])
 
-    with open(filepaths[0], newline='', encoding="utf-8") as csvfile:
-        trip_reader = list(csv.reader(csvfile, delimiter=';', quotechar='|'))
+    measurement_dictionary = meas_analyzer.get_measurement()
     
-        # for dict_key in trip_reader[0]:
-        #     if(len(dict_key) > 0):
-        #         key = dict_key.split('"')[1]
-        #         measurement_dictionary[dict_key] = None
-
-        measurement_list_first = [] 
-        measurement_list_second = []
-        measurement_list_third = [] 
-        measurement_list_fourth = [] 
-
-        measurements_list_of_lists = [] 
-
-
-        for record in trip_reader[1:]:
-            measurement_list_first.append(record[0])
-            measurement_list_second.append(record[1])
-            measurement_list_third.append(record[2])
-            measurement_list_fourth.append(record[3])
-            
-        measurement_dictionary['SECONDS'] = measurement_list_first
-        measurement_dictionary['PID'] = measurement_list_second
-        measurement_dictionary['VALUE'] = measurement_list_third
-        measurement_dictionary['UNITS'] = measurement_list_fourth
-
-    measurement_types = list(set(measurement_dictionary['PID']))
-
-    list_of_measurement_types = [[el] for el in measurement_types]
-
-    for meas_number, value in enumerate(measurement_dictionary['PID']):
-        for pid_no, elem in enumerate(measurement_types):
-            if elem == value:
-                list_of_measurement_types[pid_no].append(measurement_dictionary['VALUE'][meas_number].split('"')[1])
-    
-
-    measurement_dictionary = {}
-
-    for pid_measurement in list_of_measurement_types:
-        measurement_dictionary[pid_measurement[0]] = np.asarray([float(x) for x in pid_measurement[1:]])
-    
-
-    for key, value in measurement_dictionary.items():
-        print(f'{key}')
-    
-
     meas_list =  measurement_dictionary['"[DASH] Poziom paliwa"']
-    distance_km =  measurement_dictionary['"Pokonany dystans"']
-    obroty =  measurement_dictionary['"Oil max level"']
 
-    injection_deviation1 = measurement_dictionary['"Injection amount deviation cylinder 1"']
-    injection_deviation2 = measurement_dictionary['"Injection amount deviation cylinder 2"']
-    injection_deviation3 = measurement_dictionary['"Injection amount deviation cylinder 3"']
-    injection_deviation4 = measurement_dictionary['"Injection amount deviation cylinder 4"']
+    time_measurement = np.asarray([el[0] for el in meas_list]) / 60.0
+    value_measurement = [el[1] for el in meas_list]
+    unit_type = [el[2] for el in meas_list]
 
     plt.figure(1)
-    plt.title('Analiza przejazdu')
-    plt.plot(meas_list, label= 'Poziom paliwa[L]')
-    plt.plot(distance_km, label = 'Przebyty dystans [km]')
+    plt.title('Analiza spalania')
+    plt.plot(time_measurement, value_measurement)
+    plt.xlabel('Czas [m]')
+    plt.ylabel(f'Poziom paliwa {unit_type[0]}')
     plt.legend()
-
-    plt.figure(2)
-    plt.title('Obroty silnika')
-    plt.plot(obroty)
-    
-
-    plt.figure(3)
-    plt.title('Odchyłki wtrysków ')
-    plt.plot(injection_deviation1, label = 'Cylinder 1')
-    plt.plot(injection_deviation2, label = 'Cylinder 2')
-    plt.plot(injection_deviation3, label = 'Cylinder 3')
-    plt.plot(injection_deviation4, label = 'Cylinder 4')
-    plt.legend()
-    plt.show()
+    plt.show() 
